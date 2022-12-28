6f7cd0f2f55d0c1d93f03b9aa5b02cadf0c63e67
LucasTMB <lucas.tmb708@gmail.com>
2022-12-28 20:42:35 -0300

    Ajustando a responsividade

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Encriptador from './components/Encriptador';
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className='cabecalho'>
         <h1 className="logo-alura">
           <img src={AluraLogo} alt="Logo da Alura" />
         </h1>
